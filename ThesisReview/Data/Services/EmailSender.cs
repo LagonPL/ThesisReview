5f6db130fa3df25fd50bd673c1de2d57cc832bd2
Dawid S <lagonamv@gmail.com>
2019-08-06 14:52:18 +0200

    Small Fix

--- ThesisReview/Data/Services/EmailSender.cs
+++ ThesisReview/Data/Services/EmailSender.cs
@@ -23,7 +23,7 @@ namespace ThesisReview.Data.Services
       using (var client = new SmtpClient())
       {
         client.Connect("smtp.gmail.com", 587, false);
-        client.Authenticate("lagonamv@gmail.com", "D@wid1234");
+        client.Authenticate("lagonamv@gmail.com", "xxx");
         client.Send(message);
         client.Disconnect(true);
       }
