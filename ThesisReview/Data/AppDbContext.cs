d3852b203659513664ecd9a4dcc5df1cbd524d54
Dawid S <lagonamv@gmail.com>
2019-08-06 14:43:48 +0200

    Dodaj pliki projektów.

---
+++ ThesisReview/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThesisReview.Data
+{
+  public class AppDbContext : IdentityDbContext<IdentityUser>
+  {
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+    {
+
+    }
+
+  }
+}
