9a127da5b11ff1b5027750c47d33d45b758bf1ff
LagonPL <lagonamv@gmail.com>
2019-08-11 14:44:13 +0200

    Password Change

---
+++ ThesisReview/Controllers/SettingsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ThesisReview.Data.Models;
+using ThesisReview.ViewModels;
+
+namespace ThesisReview.Controllers
+{
+  public class SettingsController : Controller
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+
+    public SettingsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+    }
+
+    public ActionResult Account()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Account(SettingViewModel settingViewModel)
+    {
+      if (settingViewModel.NewPassword.Equals(settingViewModel.ConfirmPassword))
+      {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        if (user != null)
+        {
+          await _userManager.ChangePasswordAsync(user, settingViewModel.OldPassword, settingViewModel.NewPassword);
+        }
+      }
+      return View();
+    }
+
+  }
+}
