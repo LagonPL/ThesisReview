d76a4a2ccf439c975abcebedc882fc6489eaa0dd
LagonPL <lagonamv@gmail.com>
2019-08-11 14:44:13 +0200

    Password Change

---
+++ ThesisReview/ViewModels/SettingViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThesisReview.ViewModels
+{
+  public class SettingViewModel
+  {
+
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    public string OldPassword { get; set; }
+
+    [DataType(DataType.Password)]
+    public string NewPassword { get; set; }
+
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+
+    public bool AnyError { get; set; }
+
+  }
+}
